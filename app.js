e4bcc2cbddbf511a76eba441830c5d3cf6dde532
ArtixZ <artixzhu@gmail.com>
2017-11-13 07:01:50 -0500

    host front end bundle file with express server

--- app.js
+++ app.js
@@ -25,11 +25,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/cars', cars);
+// app.use('/', index);
+// app.use('/users', users);
+// app.use('/cars', cars);
 
 app.get('/cars/*', (req, res) => {
   const queries = req.query;
@@ -63,6 +64,11 @@ app.get('/cars/*', (req, res) => {
     }
   );
 });
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
